f343673585b17bcca635de39081b38d75cc4d4ba
NaLaura <analauradilvapereira2013@gmail.com>
2023-05-25 14:01:21 -0300

    Atualizações no codigo

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// Henrique - multiplicação
+/// Wellington - subtração
+
 int menu()
 {
     int i;
@@ -10,7 +13,7 @@ int menu()
     {
         printf("-");
     }
-    printf("\nIniciando calculadora...\n");
+    printf("\nC A L C U L A D O R A\n");
     for (i = 0; i < 30; i++)
     {
         printf("-");
@@ -18,43 +21,70 @@ int menu()
 
     sleep(3);
 
-    printf("\n1- SOMA\n");
-    printf("2- ADICAO\n");
-    printf("3- SUBTRACAO\n");
-    printf("4- DIVISAO\n");
-    printf("5- MULTIPLICACAO\n");
-    printf("6- SENO\n");
-    printf("7- COSSENO\n");
-    printf("8- TANGENTE\n");
-    printf("9- EXPONENCIAL\n");
-    printf("10- RADICIACAO\n");
-    printf("11- RAIZ QUADRADA\n");
+    // printf("Digite 2 numero: ")
+    // scanf("%d", num)
+
+    printf("1- ADICAO\n");
+    printf("2- SUBTRACAO\n");
+    printf("3- DIVISAO\n");
+    printf("4- MULTIPLICACAO\n");
+    printf("5- SENO\n");
+    printf("6- COSSENO\n");
+    printf("7- TANGENTE\n");
+    printf("8- EXPONENCIAL\n");
+    printf("9- RADICIACAO\n");
+    printf("10- RAIZ QUADRADA\n");
+
+    printf("0- SAIR\n");
 
     do
     {
         printf("Escolha o numero da operacao desejada:\n");
         scanf("%d", &resposta);
 
-        if (resposta > 11 || resposta < 1)
+        if (resposta > 10)
         {
             printf("Opcao invalida! Tente novamente.\n");
         }
 
-    } while (resposta > 11 || resposta < 1);
+        else if (resposta == 0)
+        {
+            exit(0);
+        }
+        
+
+    } while (resposta > 10);
 
     return resposta;
 }
 
 int main()
 {
-    int resposta;
-    float num, soma = 0;
+    int resposta,num1,num2;
+    int soma;
+    char escolha;
 
     resposta = menu();
 
     if (resposta == 1)
     {
-        printf("ui");
+        do
+        {
+            printf("Digite 2 numeros:\n");
+            scanf("%d", num1, num2);
+
+            soma = num1 + num2;
+            printf("O resultado e %d \n", soma);
+            printf("Deseja mudar a operacao: [S/N]\n");
+            scanf("%s",&escolha);
+        } while (escolha == 'N' || escolha == 'n');
+        
+        if (escolha == 'S' || escolha == 's')
+        {
+            menu();
+        }
+        
+
     }
 
     return 0;
